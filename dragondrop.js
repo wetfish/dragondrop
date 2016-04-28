d1f019156fa900c31b1bf2302610f1ab8dfdb23f
Rachel Fish <itsrachelfish@gmail.com>
2016-04-27 22:42:18 -0600

    Added support for loading via CommonJS and AMD

--- dragondrop.js
+++ dragondrop.js
@@ -1,3 +1,22 @@
+// Check how the plugin should be exported
+(function(factory)
+{
+    if(typeof module === 'object' && module.exports)
+    {
+        // We're in Node or a CommonJS compatable environment
+        factory(require('wetfish-basic'));
+    }
+    else if(typeof define === 'function' && define.amd)
+    {
+        // We're in a browser being loaded with AMD (Require.js)
+        define(['wetfish-basic'], factory);
+    }
+    else
+    {
+        // We're in a browser, so assume everything has been loaded as global variables
+        factory(basic);
+    }
+}
 (function($)
 {
     var Dragon = function(element, options)
@@ -349,4 +368,4 @@
             new Dragon(element, options);
         });
     }
-}(basic));
+}));
